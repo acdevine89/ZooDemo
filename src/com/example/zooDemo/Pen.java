b4f50e4ed94e395cad747f8cf69b677989dac885
acdevine89 <annie.devine@detroitlabs.com>
2014-09-29 10:16:34 -0400

    Adding lists for animals and baby animals to Pen class

--- src/com/example/zooDemo/Pen.java
+++ src/com/example/zooDemo/Pen.java
@@ -1,8 +1,13 @@
 package com.example.zooDemo;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by anniedevine on 9/29/14.
  */
 public class Pen {
+
+    List<Animal> allTheAnimals = new ArrayList<Animal>();
+    List<BabyAnimal> allTheBabyAnimals = new ArrayList<BabyAnimal>();
+
 }
