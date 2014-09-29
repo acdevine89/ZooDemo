1c608b6dab18fd6feaf618a3627248f262d4896f
acdevine89 <annie.devine@detroitlabs.com>
2014-09-29 10:05:23 -0400

    Adding instance variables to Animal class

--- src/com/example/zooDemo/Animal.java
+++ src/com/example/zooDemo/Animal.java
@@ -4,4 +4,9 @@ package com.example.zooDemo;
  * Created by anniedevine on 9/29/14.
  */
 public class Animal {
+
+    private String species;
+    private String size;
+    private char gender;
+
 }
