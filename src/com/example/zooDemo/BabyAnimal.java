7f5b079fabbee89bbd49548c16bcec1c00fc628c
acdevine89 <annie.devine@detroitlabs.com>
2014-09-29 10:13:10 -0400

    Adding BabyAnimal instance variable and parameterized constructor with super

--- src/com/example/zooDemo/BabyAnimal.java
+++ src/com/example/zooDemo/BabyAnimal.java
@@ -3,8 +3,15 @@ package com.example.zooDemo;
 /**
  * Created by anniedevine on 9/29/14.
  */
-public class BabyAnimal {
+public class BabyAnimal extends Animal {
 
+    private boolean hasSiblingsOrNot;
+
+    public BabyAnimal(boolean hasSiblingsOrNot, String species, int size, char gender)
+    {
+        super(species, size, gender);
+        this.hasSiblingsOrNot = hasSiblingsOrNot;
+    }
 
 
 }
