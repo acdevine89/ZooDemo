3862d97765580260ec9368635516d55d0c1c98a4
acdevine89 <annie.devine@detroitlabs.com>
2014-09-29 10:18:20 -0400

    Adding list of pens to Zoo class

--- src/com/example/zooDemo/Zoo.java
+++ src/com/example/zooDemo/Zoo.java
@@ -1,7 +1,12 @@
 package com.example.zooDemo;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by anniedevine on 9/29/14.
  */
 public class Zoo {
+
+    List<Pen> allThePens = new ArrayList<Pen>();
+
 }
